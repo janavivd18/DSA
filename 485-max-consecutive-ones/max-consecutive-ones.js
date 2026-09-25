e2fdcb3dfa72fe64cd851f6b5adbb2727dce9f0a
Janavi vd <janavivd18@gmail.com>
2026-09-25 19:25:18 +0530

    Time: 2 ms (66.05%) | Memory: 60.8 MB (13.92%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count=0;
+    let max=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===1){
+            count++;
+            if(count>max){
+                max=count;
+            }
+        }else{
+            count=0;
+        }
+    }
+    return max;
+}
+nums=[1,1,0,1,1,1];
+console.log(findMaxConsecutiveOnes(nums));
+
+    
